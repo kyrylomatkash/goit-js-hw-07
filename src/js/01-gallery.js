a21822641a4d54e418a79acdd8061833c76d91a0
Kyrylo <kyrylo.mm@gmail.com>
2023-10-20 03:42:29 +0200

    Add all modules and all correct code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const galleryTemplate = galleryItems.map(({ preview, original, description }) => {
+  return `<li class='gallery__item'>
+    <a class='gallery__link' href='${original}'>
+      <img class='gallery__image' src='${preview}' data-source='${original}' alt='${description}'/>
+    </a>
+  </li>`;
+}).join("");
+gallery.insertAdjacentHTML("afterbegin", galleryTemplate);
+gallery.addEventListener("click", imageListener);
+function imageListener(event) {
+  event.preventDefault();
+  const imageClickerTarget = event.target;
+  if (imageClickerTarget.classList.contains('gallery__image')) {
+    const primaryImageUrl = imageClickerTarget.dataset.source;
+    const modal = basicLightbox.create(`<img src="${primaryImageUrl}" width="600" height="600"/>`);
+    modal.show();
+    document.addEventListener("keydown", closeOnEscape);
+    function closeOnEscape(event) {
+      if (event.key === "Escape") {
+        modal.close();
+        document.removeEventListener("keydown", closeOnEscape);
+      }
+    }
+    modal.on("afterClose", () => {
+      document.removeEventListener("keydown", closeOnEscape);
+    });
+  }
+}
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+const gallery = document.querySelector('.gallery');
+const galleryTemplate = galleryItems.map(({ preview, original, description }) => {
+  return `<li class='gallery__item'>
+   <a class='gallery__link' href='${original}'>
+      <img class='gallery__image' src='${preview}' alt='${description}' />
+   </a>
+</li>`;
+}).join('');
+gallery.insertAdjacentHTML('beforeend', galleryTemplate);
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    fadeSpeed: 250,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    close: true,
+    closeText: '×',
+    nav: true,
+  navText: ['←', '→'],
+  download: 'Click to download button',
+  });
 console.log(galleryItems);
+
+
